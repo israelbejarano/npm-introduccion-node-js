b40e786d6ac7d97ed1c08d3a1d12edf3b0d0b83e
israel <israel.bejarano.a@gmail.com>
2019-10-23 17:13:13 +0200

    index listo

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log('Hola Mundo!');
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`Hola ${nombre}`);
+    },
+    holaCursoNPM: function() {
+        console.log('Hola curso NPM');
+    }
+}
